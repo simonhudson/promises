4a42485195614317960e77754f634046d2a4cb05
Simon Hudson <hellosimonhudson+github@gmail.com>
2017-09-06 13:17:41 +0100

    Remove some logs()

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 'use strict';
 
+const element = document.getElementById('js-data');
+
 const renderData = (data) => {
+	element.textContent = 'Complete';
 	console.table(data);
 };
 
-const getData = () => {
+const getData = (callback) => {
 	fetch('https://swapi.co/api/people/')
 		.then(response => response.json())
-		.then(data => renderData(data.results))
+		.then(data => callback(data.results))
 		.catch(error => console.log(error));
 };
 
-getData();
-console.log(1);
-console.log(2);
+getData(renderData);
