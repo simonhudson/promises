ba9b78befd72c7690ade4c1d953e2419c1750957
Simon Hudson <hellosimonhudson+github@gmail.com>
2017-11-09 12:22:07 +0000

    Add loading img and additional content element

--- main.js
+++ main.js
@@ -1,17 +1,51 @@
 'use strict';
 
-const element = document.getElementById('js-data');
-
-const renderData = (data) => {
-	element.textContent = 'Complete';
-	console.table(data);
+const toggleLoading = (element, isLoading = false) => {
+	const method = isLoading ? 'remove' : 'add';
+	element.querySelector('.js-loading-img').classList[method]('hidden');
 };
 
-const getData = (callback) => {
-	fetch('https://swapi.co/api/people/')
+const transform = {
+
+	people(data) {
+		const returnData = [];
+		data.forEach(item => returnData.push({name: item.name}));
+		return returnData;
+	}
+
+};
+
+const render = {
+
+	people(data, element) {
+		data.forEach(item => {
+			const nameElement = document.createElement('p');
+			nameElement.innerHTML = item.name;
+			element.appendChild(nameElement);
+		});
+	},
+	planets(data, element) {
+		data.forEach(item => {
+			const nameElement = document.createElement('p');
+			nameElement.innerHTML = item.name;
+			element.appendChild(nameElement);
+		});
+	},
+
+};
+
+const elements = Array.from(document.querySelectorAll('.js-data')) || null;
+
+const renderData = (element, endpoint, data) => {
+	render[endpoint](data, element);
+	toggleLoading(element);
+};
+
+elements.forEach(element => {
+	const endpoint = element.dataset.apiEndpoint;
+	fetch(`https://swapi.co/api/${endpoint}`)
 		.then(response => response.json())
-		.then(data => callback(data.results))
+		.then(data => renderData(element, endpoint, data.results))
 		.catch(error => console.log(error));
-};
 
-getData(renderData);
+});
